7cd94b0ca594864fb4d9ac313e2de180b47005dc
yanthy <1297032395@qq.com>
2020-06-13 17:24:25 +0800

    'logback日志添加'

--- src/test/java/com/imooc/sell/dao/ProductCategoryDaoTest.java
+++ src/test/java/com/imooc/sell/dao/ProductCategoryDaoTest.java
@@ -1,6 +1,7 @@
 package com.imooc.sell.dao;
 
 import com.imooc.sell.entity.ProductCategory;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class ProductCategoryDaoTest {
     @Autowired
     ProductCategoryDao productCategoryDao;
@@ -16,6 +18,7 @@ public class ProductCategoryDaoTest {
     @Test
     public void test01(){
         System.out.println("111111111111");
+        log.info("info");
         ProductCategory productCategory = productCategoryDao.findOne(1);
         System.out.println(productCategory);
     }
